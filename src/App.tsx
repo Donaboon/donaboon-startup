d91605da1f493e118ddcb6e5d5b76b7f4e18da06
Xameon <andrej.shmalenko@gmail.com>
2025-09-23 19:42:39 +0300

    Redesigned.

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,10 @@ import {
   HStack,
   IconButton,
   Link,
+  Icon,
 } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
+import { DonaboonLogoIcon } from './DonaboonLogo';
 import i18n from './i18n';
 
 function App() {
@@ -83,234 +85,200 @@ function App() {
   };
 
   return (
-    <VStack>
+    <VStack
+      w='100vw'
+      height='100svh'
+      backgroundImage='url(background.jpg)'
+      backgroundSize='cover'
+      alignItems='left'
+      backgroundPosition='right center'
+    >
       <VStack
-        w='100vw'
-        height='100vh'
-        display='flex'
-        flexDirection='column'
-        backgroundImage='url(background.jpg)'
-        backgroundSize='cover'
-        alignItems='left'
-        backgroundPosition='right center'
-        backgroundRepeat='no-repeat'
+        w='full'
+        height='full'
+        maxW='xl'
+        justifyContent='space-between'
+        p='10'
+        bg='teal.700/50'
+        backdropFilter='blur(3px)'
+        boxShadow='2xl'
       >
-        <VStack
-          w='100%'
-          maxW='600px'
-          height='100vh'
-          justifyContent='space-between'
-          alignItems='left'
-          pl={{ base: 5, sm: 10 }}
-          pr={{ base: 5, sm: 10, md: 0 }}
-        >
-          <VStack
-            direction='row'
-            alignItems='center'
-            gap='10px'
-            filter='brightness(10000%)'
-            mt='50px'
-          >
+        <VStack alignItems='center' w='full'>
+          <HStack align='center'>
             <Text
-              fontWeight={600}
-              color='white'
-              fontSize={{ base: 40, sm: 50 }}
-              lineHeight={1}
+              fontWeight='bold'
+              color='teal.50'
+              fontSize={{ base: '6xl', smDown: '5xl' }}
+              lineHeight={0.5}
             >
               {t('DONABOON')}
             </Text>
-            <HStack alignItems='start' h='60px' ml={20}>
-              <Image
-                src='logo.png'
-                alt='Donaboon Logo'
-                width={{ base: 61, sm: 81 }}
-                borderRadius='8px'
-                mr={0}
-              />
-              <Text color='white' fontSize={{ base: 20, sm: 25 }}>
-                {t('COMING_SOON')}
-              </Text>
-            </HStack>
-          </VStack>
-
-          <VStack
-            textAlign='center'
-            maxWidth='800px'
-            color='white'
-            pl={{ base: 0, md: '20px' }}
-            marginInline={{ base: '10px', sm: '0' }}
+            <Icon
+              as={DonaboonLogoIcon}
+              css={{
+                width: { base: '4rem', smDown: '3.5rem' },
+              }}
+              stroke='white'
+              fill='teal.50'
+            />
+          </HStack>
+          <Text
+            color='teal.50'
+            fontSize={{ base: '2xl', smDown: 'xl' }}
+            letterSpacing={1.5}
+            textAlign='start'
           >
-            <Text
-              fontWeight={600}
-              lineHeight={1.3}
-              textShadow='2px 2px 4px rgba(0,0,0,0.3)'
-              maxWidth='600px'
-              mx='auto'
-              fontSize={{ base: 23, sm: 27 }}
-            >
+            {t('COMING_SOON')}
+          </Text>
+        </VStack>
+        <VStack textAlign='center' color='teal.50' gap='8'>
+          <VStack gap='0'>
+            <Text fontWeight='bold' fontSize={{ base: '2xl', smDown: 'xl' }}>
               {t('JOIN_OUR_JOURNEY')}
             </Text>
-            <Text
-              as='h4'
-              mb={4}
-              lineHeight={1.3}
-              textShadow='2px 2px 4px rgba(0,0,0,0.3)'
-              maxWidth='600px'
-              mx='auto'
-              fontSize={{ base: 16, sm: 22 }}
-            >
+            <Text fontWeight='semibold' fontSize={{ base: 'xl', smDown: 'lg' }}>
               {t('LEAVE_EMAIL_LAUNCH_NOTIFY')}
             </Text>
-
-            <HStack
-              w='full'
-              gap={{ base: 2, sm: 5 }}
-              pl={{ base: 0, sm: '10px' }}
-              flexDirection={{ base: 'column', md: 'row' }}
-            >
-              <Input
-                placeholder={t('YOUR_EMAIL_ADDRESS')}
-                type='email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                bg='#00000040'
-                h={{ base: '40px', sm: '60px' }}
-                borderRadius={{ base: '10px', sm: '20px' }}
-                color='white'
-                _placeholder={{ color: 'white', textAlign: 'center' }}
-                fontSize={{ base: 13, sm: 21 }}
-                fontWeight={400}
-                padding='10px 14px'
-                flex='1'
-                border='none'
-                outline='none'
-                textAlign='center'
-                _hover={{ outline: '5px solid #0d9488' }}
-              />
-              <Button
-                onClick={handleSubmit}
-                bg='#00000040'
-                color='white'
-                padding='12px 24px'
-                h={{ base: '40px', sm: '60px' }}
-                borderRadius={{ base: '10px', sm: '20px' }}
-                width='auto'
-                fontSize={{ base: 12, sm: 17 }}
-                fontWeight={100}
-                p={2}
-                _hover={{ outline: '5px solid #0d9488' }}
-              >
-                {t('NOTIFY_ME')}
-              </Button>
-            </HStack>
-            <Text color='white' fontWeight={500} m='none' h={10}>
-              {message ?? ''}
-            </Text>
           </VStack>
 
-          <VStack
-            color='white'
-            alignItems='start'
-            pl={{ base: 0, md: 5 }}
-            gap={3}
+          <HStack
+            w='full'
+            gap={{ base: '4', smDown: '3' }}
+            flexDirection={{ base: 'column', md: 'row' }}
           >
-            <HStack
-              gap={2}
-              fontSize={{ base: 12, sm: 17 }}
-              alignItems='end'
-              pl={{ base: 3, md: 8 }}
+            <Input
+              placeholder={t('YOUR_EMAIL_ADDRESS')}
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              textAlign='center'
+              colorPalette='teal'
+              size={{ base: 'xl', smDown: 'lg' }}
+              variant='subtle'
+              bgColor='teal.600/90'
+              color='teal.50'
+              fontSize={{ base: 'xl', smDown: 'lg' }}
+              _placeholder={{ color: 'teal.100' }}
+              outlineColor='teal.50'
+              outlineWidth='2px'
+              transition='all 0.2s'
+              _hover={{ boxShadow: 'md', transform: 'translateY(-1px)' }}
+              _active={{ boxShadow: 'md', transform: 'translateY(-1px)' }}
+              _focus={{ boxShadow: 'md', transform: 'translateY(-1px)' }}
+              rounded='xl'
+              w='full'
+            />
+            <Button
+              onClick={handleSubmit}
+              fontSize={{ base: 'xl', smDown: 'lg' }}
+              colorPalette='teal'
+              size={{ base: 'xl', smDown: 'lg' }}
+              variant={'surface'}
+              rounded='xl'
+              w={{ base: 'auto', mdDown: 'full' }}
             >
-              <Link
-                href='https://www.facebook.com/donaboon.official/'
-                target='_blank'
-                rel='noopener noreferrer'
-                color='white'
-                alignItems='end'
-              >
-                <Image
-                  src='facebook.png'
-                  alt='Facebook'
-                  w={{ base: 15, sm: 19 }}
-                />
-                <Text fontWeight={600}>{t('EVERYDAY_NEWS')}</Text>
-              </Link>
-            </HStack>
-            <HStack gap={3} pl={{ base: 0, sm: 5 }}>
-              <HStack
-                bg='#C0E0D3'
-                gap={3}
-                borderRadius={60}
-                p={{ base: 1, sm: 2 }}
-                paddingInline={{ base: 4, sm: 5 }}
-              >
-                <IconButton
-                  onClick={() => handleLanguageChange('en')}
-                  aria-label='Switch to English'
-                  border='none'
-                  background='none'
-                >
-                  <Image
-                    src='usa.png'
-                    alt='US Flag'
-                    w={{ base: 41, sm: 50 }}
-                    borderRadius={2}
-                    _hover={{ outline: '5px solid #0d9488' }}
-                  />
-                </IconButton>
-                <IconButton
-                  onClick={() => handleLanguageChange('ua')}
-                  aria-label='Switch to Ukrainian'
-                  border='none'
-                  background='none'
-                >
-                  <Image
-                    src='ukraine.png'
-                    alt='Ukraine Flag'
-                    w={{ base: 41, sm: 50 }}
-                    borderRadius={2}
-                    _hover={{ outline: '5px solid #0d9488' }}
-                  />
-                </IconButton>
-                <IconButton
-                  onClick={() => handleLanguageChange('ro')}
-                  aria-label='Switch to Romanian'
-                  border='none'
-                  background='none'
-                >
-                  <Image
-                    src='romania.png'
-                    alt='Romania Flag'
-                    w={{ base: 41, sm: 50 }}
-                    borderRadius={2}
-                    _hover={{ outline: '5px solid #0d9488' }}
-                  />
-                </IconButton>
-              </HStack>
+              {t('NOTIFY_ME')}
+            </Button>
+          </HStack>
+          <Text color='white' fontWeight={500} m='none' h={10}>
+            {message ?? ''}
+          </Text>
+        </VStack>
 
-              <a
-                href='https://www.linkedin.com/in/donaboon-ro-45220837b/'
-                target='_blank'
-                rel='noopener noreferrer'
+        <VStack color='white' alignItems='center' gap={3}>
+          <HStack gap={2} fontSize={{ base: 12, sm: 17 }} alignItems='end'>
+            <Link
+              href='https://www.facebook.com/donaboon.official/'
+              target='_blank'
+              rel='noopener noreferrer'
+              color='white'
+              alignItems='end'
+            >
+              <Image
+                src='facebook.png'
+                alt='Facebook'
+                w={{ base: 15, sm: 19 }}
+              />
+              <Text fontWeight={600}>{t('EVERYDAY_NEWS')}</Text>
+            </Link>
+          </HStack>
+          <HStack gap={4}>
+            <HStack
+              bg='#C0E0D3'
+              gap={3}
+              rounded='xl'
+              p={{ base: 1, sm: 2 }}
+              paddingInline={{ base: 4, sm: 5 }}
+            >
+              <IconButton
+                onClick={() => handleLanguageChange('en')}
+                aria-label='Switch to English'
+                border='none'
+                background='none'
               >
                 <Image
-                  src='linkedin.png'
-                  alt='LinkedIn'
-                  w={{ base: 33, sm: 41 }}
+                  src='usa.png'
+                  alt='US Flag'
+                  w={{ base: 41, sm: 50 }}
+                  borderRadius={2}
+                  transition='all 0.2s'
+                  _hover={{ boxShadow: 'md', transform: 'translateY(-1px)' }}
                 />
-              </a>
-              <a
-                href='https://www.instagram.com/donaboon.official/'
-                target='_blank'
-                rel='noopener noreferrer'
+              </IconButton>
+              <IconButton
+                onClick={() => handleLanguageChange('ua')}
+                aria-label='Switch to Ukrainian'
+                border='none'
+                background='none'
               >
                 <Image
-                  src='instagram.png'
-                  alt='Instagram'
-                  w={{ base: 33, sm: 37 }}
+                  src='ukraine.png'
+                  alt='Ukraine Flag'
+                  w={{ base: 41, sm: 50 }}
+                  borderRadius={2}
+                  transition='all 0.2s'
+                  _hover={{ boxShadow: 'md', transform: 'translateY(-1px)' }}
                 />
-              </a>
+              </IconButton>
+              <IconButton
+                onClick={() => handleLanguageChange('ro')}
+                aria-label='Switch to Romanian'
+                border='none'
+                background='none'
+              >
+                <Image
+                  src='romania.png'
+                  alt='Romania Flag'
+                  w={{ base: 41, sm: 50 }}
+                  transition='all 0.2s'
+                  _hover={{ boxShadow: 'md', transform: 'translateY(-1px)' }}
+                />
+              </IconButton>
             </HStack>
-          </VStack>
+
+            <a
+              href='https://www.linkedin.com/in/donaboon-ro-45220837b/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <Image
+                src='linkedin.png'
+                alt='LinkedIn'
+                w={{ base: 33, sm: 41 }}
+              />
+            </a>
+            <a
+              href='https://www.instagram.com/donaboon.official/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <Image
+                src='instagram.png'
+                alt='Instagram'
+                w={{ base: 33, sm: 37 }}
+              />
+            </a>
+          </HStack>
         </VStack>
       </VStack>
     </VStack>
